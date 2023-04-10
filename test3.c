03aa9fb5e54f7c4a7e4e39a48151841b37cde2fb
Lee-jihwan-10 <130123487+Lee-jihwan-10@users.noreply.github.com>
2023-04-10 10:44:50 +0900

    Add files via upload

---
+++ test3.c
@@ -0,0 +1,17 @@
+//  #include <stdio.h>
+
+// int main(){
+//     int a, sum = 0 ;
+//     scanf("%d",a);
+//     for (int i=0 ;i<=a;i++ ){
+//         sum += i ;
+//     }
+//     printf("%d",sum);
+//     return 0;
+// }
+
+#include <stdio.h>
+void main(){
+    float i =12.8;
+    printf("%8.3f",i);
+}
